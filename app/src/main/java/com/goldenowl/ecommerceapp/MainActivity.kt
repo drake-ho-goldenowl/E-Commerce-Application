cc45dff652223c20f8487f463276a63b5bc03422
Drake Ho <drake.ho.goldenowl@gmail.com>
2022-05-10 16:46:14 +0700

    Build App Struture Bottom Navigation

--- app/src/main/java/com/goldenowl/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/goldenowl/ecommerceapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.goldenowl.ecommerceapp
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = supportActionBar
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+        setupBottomNavMenu(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    toolbar?.title = "Home"
+                }
+                R.id.shopFragment -> {
+                    toolbar?.title = "Shop"
+                }
+                R.id.bagFragment -> {
+                    toolbar?.title = "Bag"
+                }
+                R.id.favoritesFragment -> {
+                    toolbar?.title = "Favorites"
+                }
+                R.id.profileFragment -> {
+                    toolbar?.title = "Profile"
+                }
+            }
+        }
     }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav?.setupWithNavController(navController)
+    }
+
 }
