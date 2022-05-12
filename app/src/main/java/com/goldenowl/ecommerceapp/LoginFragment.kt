e8e0f234e0f9b31e128602dac67517bb1412e3ae
Drake Ho <drake.ho.goldenowl@gmail.com>
2022-05-12 12:24:37 +0700

    Check authorized and navigator

--- app/src/main/java/com/goldenowl/ecommerceapp/LoginFragment.kt
+++ app/src/main/java/com/goldenowl/ecommerceapp/LoginFragment.kt
@@ -1,14 +1,18 @@
 package com.goldenowl.ecommerceapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import com.goldenowl.ecommerceapp.utilities.PASSWORD
+import com.goldenowl.ecommerceapp.utilities.USER_NAME
 
 class LoginFragment : Fragment() {
 
@@ -22,8 +26,31 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val txtUserName = view.findViewById<EditText>(R.id.txtUserName)
+        val txtPassword = view.findViewById<EditText>(R.id.txtPassword)
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val userName =  sharedPref.getString(USER_NAME,null)
+        val userPassword = sharedPref.getString(PASSWORD,null)
+        println("$userName $userPassword")
+        if(userName != null && userPassword != null){
+            txtUserName.setText(userName.toString())
+            txtPassword.setText(userPassword.toString())
+        }
 
         view.findViewById<Button>(R.id.btnLogin).setOnClickListener{
+
+//            with (sharedPref.edit()) {
+//                this.putString(USER_NAME, txtUserName.text.toString())
+//                this.putString(PASSWORD, txtPassword.text.toString())
+//            }
+
+            sharedPref.edit().apply{
+                putString(USER_NAME,txtUserName.text.toString())
+                putString(PASSWORD,txtPassword.text.toString())
+            }.apply()
+
+
             startActivity(Intent(activity,MainActivity::class.java))
             activity?.finish()
         }
@@ -35,4 +62,5 @@ class LoginFragment : Fragment() {
             }
         }
     }
+
 }
---
+++ app/src/main/java/com/goldenowl/ecommerceapp/utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.goldenowl.ecommerceapp.utilities
+
+const val USER_NAME = "USERNAME"
+const val PASSWORD = "PASSWORD"
