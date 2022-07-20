15c1f23bb7ea587d862483ad6e29f9c99b04045e
Drake Ho <drake.ho.goldenowl@gmail.com>
2022-07-20 14:46:07 +0700

    Update filter and refactor code

--- app/src/main/java/com/goldenowl/ecommerceapp/utilities/GlideDefault.kt
+++ app/src/main/java/com/goldenowl/ecommerceapp/utilities/GlideDefault.kt
@@ -60,7 +60,7 @@ internal object GlideDefault {
     private fun createCircularProgress(context: Context): CircularProgressDrawable {
         val circularProgressDrawable = CircularProgressDrawable(context)
         circularProgressDrawable.strokeWidth = 5f
-        circularProgressDrawable.centerRadius = 50f
+        circularProgressDrawable.centerRadius = 30f
         circularProgressDrawable.start()
         return circularProgressDrawable
     }
